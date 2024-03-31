37bf879c988ab4211f23177eb89349900c948ebd
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-04-01 02:30:24 +0530

    Update __init__.py

--- STORM/__init__.py
+++ STORM/__init__.py
@@ -8,7 +8,7 @@ from typing import Any, Dict
 
 
 from aiohttp import ClientSession
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler import AsyncIOScheduler
 from gpytranslate import Translator
 from pyrogram import Client
 from pytgcalls import GroupCallFactory
