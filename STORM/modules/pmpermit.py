9b596459ef6a15c1964e4eba71946c46cab66768
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-04-04 13:30:56 +0530

    Update pmpermit.py

--- STORM/modules/pmpermit.py
+++ STORM/modules/pmpermit.py
@@ -9,7 +9,7 @@ KEX = PM_PIC
 TEXT = """**Hey, I am an advanced & superfast high quality userbot assistant with an upgraded version security system.\n
 ╰• **Owner** » {}
 ➖➖➖➖➖➖➖➖➖➖➖ 
-My owner is offline now, please wait until my owner allows you.\nPlease don't spam here, because spamming will **force me to block you from my owner id.**\n
+**My owner is offline now, please wait until my owner allows you.\n Please don't spam here, because spamming will **force me to block you from my owner id.**\n
 • **Warn Limits** » {}      
 ╰• **Your Warns** » {}
 ➖➖➖➖➖➖➖➖➖➖➖
