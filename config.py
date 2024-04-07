d8be071096e92589b75f0c16ded6aa2a5c4338d0
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-04-07 14:35:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from STORMDB.data import STORMS
 API_ID = int(getenv("API_ID", "25981592"))
 API_HASH = getenv("API_HASH", "709f3c9d34d83873d3c7e76cdd75b866")
 SESSION1 = getenv("SESSION")
-BOT_TOKEN = getenv("BOT_TOKEN"), "")
+BOT_TOKEN = getenv("BOT_TOKEN"), ""))
 ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/5d4a2dbf4f196fcdfe4d2.mp4")
 HELP_PIC = getenv("HELP_PIC", "https://graph.org/file/e0fcedd2df8ac254bb506.jpg")
 OWNER_ID = int(getenv("OWNER_ID", "6257927828"))
