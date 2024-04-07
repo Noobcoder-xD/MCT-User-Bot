34f59eb0eda28685debddf200cec059f68f39153
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-04-07 14:13:05 +0530

    Update helmp.py

--- STORM/modules/helmp.py
+++ STORM/modules/helmp.py
@@ -4,9 +4,9 @@ from prettytable import PrettyTable
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
-from Zaid import app, CMD_HELP
-from Zaid.helper.PyroHelpers import ReplyCheck
-from Zaid.helper.utility import split_list
+from STORM import app, CMD_HELP
+from STORM.helper.PyroHelpers import ReplyCheck
+from STORM.helper.utility import split_list
 
 
 async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
